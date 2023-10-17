2b9eacd973b3ca4b19e233d58c4da99afe27213c
Collins Daberechukwu <collinsdaberechi20@gmail.com>
2023-10-17 23:16:18 +0100

    Admin service update

--- src/main/java/com/example/demomvcedoclass/service/AdminService.java
+++ src/main/java/com/example/demomvcedoclass/service/AdminService.java
@@ -1,14 +1,19 @@
 package com.example.demomvcedoclass.service;
 
+import com.example.demomvcedoclass.dto.ProductsDto;
+import com.example.demomvcedoclass.model.Products;
 import com.example.demomvcedoclass.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-
-
+@RequiredArgsConstructor
 @Service
 public class AdminService {
     private final ProductRepository product;
 
-    public AdminService(ProductRepository product) {
-        this.product = product;
+    public ResponseEntity<Products> createProduct(ProductsDto product){
+        Products newProduct = new Products();
+        newProduct.setProductName(product.getProductName());
+        newProduct.setdescription(product.getdescription());
     }
 }
